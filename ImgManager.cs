c404b9cf7ccf4750fb76fe99d0ac12b65cb2a6c9
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-29 15:24:56 -0300

    Criação da classe para auxilio no salvar e ler imagens

---
+++ ImgManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shorthander
+{
+    public class ImgManager
+    {
+        public Bitmap ReadImage(string path)
+        {
+            return (Bitmap)Image.FromFile(path);
+        }
+
+        public void WriteImage(Bitmap image, string path)
+        {
+            var file = new string(path);
+            if (file.EndsWith(".jpg")) file = file.Replace(".jpg", ".jpeg");
+            var format = typeof(ImageFormat).GetProperties().SingleOrDefault(x => "." + x.Name.ToLower() == Path.GetExtension(file).ToLower());
+            if (format == null) throw new FileFormatException(new Uri(file), "Extensão não suportada!");
+
+            if (File.Exists(path))
+                File.Delete(path);
+            image.Save(path, (ImageFormat)format.GetValue(null, null));
+        }
+    }
+}
