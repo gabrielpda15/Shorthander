b852bf7dfd4b124e80d235f683e0cd3ef25de27c
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-29 15:25:12 -0300

    Criação da classe da dados estáticos

---
+++ StaticData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shorthander
+{
+    public static class StaticData
+    {
+        public const string MAGIC = "(Shorthander)";
+
+        public static readonly string[] HEADER = new string[]
+        {
+            "&F1 #####################################################################&NL",
+            "&F1 #                                                                   #&NL",
+            "&F1 #                           SHORTHANDER                             #&NL",
+            "&F1 #                                                                   #&NL",
+            "&F1 #####################################################################&NL&NL"
+        };
+
+        public static readonly string CURSOR = " @> ";
+
+    }
+}
