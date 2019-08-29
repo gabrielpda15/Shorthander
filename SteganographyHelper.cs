2b350876ad2a5058571e973669395b4304a45ccb
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-29 15:25:44 -0300

    Criação da classe para efetuar a esteganografia da imagem

---
+++ SteganographyHelper.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Shorthander
+{
+    public static class SteganographyHelper
+    {
+        enum State
+        {
+            Hiding,
+            FillingWithZeros
+        };
+
+        public static Bitmap EmbedText(string text, Bitmap bmp)
+        {
+            State s = State.Hiding;
+
+            int charIndex = 0;
+            int charValue = 0;
+            long colorUnitIndex = 0;
+
+            int zeros = 0;
+
+            int R = 0, G = 0, B = 0;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    pixel = Color.FromArgb(
+                        pixel.R - pixel.R % 2,
+                        pixel.G - pixel.G % 2, 
+                        pixel.B - pixel.B % 2);
+
+                    R = pixel.R; 
+                    G = pixel.G; 
+                    B = pixel.B;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            if (zeros == 8)
+                            {
+                                if ((colorUnitIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+
+                                return bmp;
+                            }
+
+                            if (charIndex >= text.Length)
+                            {
+                                s = State.FillingWithZeros;
+                            }
+                            else
+                            {
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (s == State.Hiding)
+                                    {
+                                        R += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (s == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (s == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+
+                        colorUnitIndex++;
+
+                        if (s == State.FillingWithZeros)
+                        {
+                            zeros++;
+                        }
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+        public static string ExtractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            string extractedText = String.Empty;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+
+                        colorUnitIndex++;
+
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            charValue = ReverseBits(charValue);
+
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+
+                            char c = (char)charValue;
+
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        public static int ReverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+
+    }
+}
